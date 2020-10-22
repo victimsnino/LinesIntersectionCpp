7d7308e30dc2a52c6e9cfe03843ce289e4890898
Aleksey Loginov <victimsnino@gmail.com>
2020-10-22 16:43:49 +0300

    Remove redunant namespace

--- avl/avl_node.h
+++ avl/avl_node.h
@@ -58,9 +58,9 @@ private:
 template<typename KeyType>
 Node<KeyType>* Node<KeyType>::Insert(KeyType value)
 {
-    auto& ptr = value < BaseNode<KeyType>::GetValue() ?
-                    BaseNode<KeyType>::GetLeftNode() :
-                    BaseNode<KeyType>::GetRightNode();
+    auto& ptr = value < GetValue() ?
+                    GetLeftNode() :
+                    GetRightNode();
     if (!ptr)
         ptr = new Node<KeyType>(std::move(value));
     else
@@ -72,7 +72,7 @@ Node<KeyType>* Node<KeyType>::Insert(KeyType value)
 template<typename KeyType>
 void Node<KeyType>::UpdateHeight()
 {
-    m_height = std::max(GetHeight(BaseNode<KeyType>::GetLeftNode()), GetHeight(BaseNode<KeyType>::GetRightNode())) + 1;
+    m_height = std::max(GetHeight(GetLeftNode()), GetHeight(GetRightNode())) + 1;
 }
 
 template<typename KeyType>
@@ -85,12 +85,13 @@ Node<KeyType>* Node<KeyType>::BalanceIfNeeded()
 
     if (diff_heights == 2) // right child height > left child
     {
-        Node<KeyType>*& right_node = BaseNode<KeyType>::GetRightNode();
-        if (right_node->GetDiffHeight() < 0) // his left is longer, then we need to do big left rotate (rotate right child  right and then left self)
+        Node<KeyType>*& right_node = GetRightNode();
+        if (right_node->GetDiffHeight() < 0)
+            // his left is longer, then we need to do big left rotate (rotate right child  right and then left self)
             right_node = right_node->RotateRight();
         return RotateLeft();
     }
-    Node<KeyType>*& left_node = BaseNode<KeyType>::GetLeftNode();
+    Node<KeyType>*& left_node = GetLeftNode();
     if (left_node->GetDiffHeight() > 0)
         left_node = left_node->RotateLeft();
     return RotateRight();
@@ -99,9 +100,9 @@ Node<KeyType>* Node<KeyType>::BalanceIfNeeded()
 template<typename KeyType>
 Node<KeyType>* Node<KeyType>::RotateLeft()
 {
-    Node<KeyType>* right_child        = BaseNode<KeyType>::GetRightNode();
-    BaseNode<KeyType>::GetRightNode() = right_child->GetLeftNode();
-    right_child->GetLeftNode()        = this;
+    Node<KeyType>* right_child = GetRightNode();
+    GetRightNode()             = right_child->GetLeftNode();
+    right_child->GetLeftNode() = this;
 
     UpdateHeight();
     right_child->UpdateHeight();
@@ -111,9 +112,9 @@ Node<KeyType>* Node<KeyType>::RotateLeft()
 template<typename KeyType>
 Node<KeyType>* Node<KeyType>::RotateRight()
 {
-    Node<KeyType>* left_child        = BaseNode<KeyType>::GetLeftNode();
-    BaseNode<KeyType>::GetLeftNode() = left_child->GetRightNode();
-    left_child->GetRightNode()       = this;
+    Node<KeyType>* left_child  = GetLeftNode();
+    GetLeftNode()              = left_child->GetRightNode();
+    left_child->GetRightNode() = this;
 
     UpdateHeight();
     left_child->UpdateHeight();
